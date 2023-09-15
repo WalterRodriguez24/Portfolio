01ca251a2b41a1aaefc4bc20cda15063fbd7cd1d
Walter rodriguez <rodriguezlopezwalter24@gmail.com>
2023-09-14 23:47:27 -0500

    WR

--- components/Transition.js
+++ components/Transition.js
@@ -27,7 +27,7 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+        transition={{ delay: 0.1, duration: 0.2, ease: "easeInOut" }}
       ></motion.div>
       <motion.div
         className="fixed top-0 botton-0 right-full w-screen h-screen z-20 bg-[#3b2d71]"
@@ -35,7 +35,7 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
+        transition={{ delay: 0.2, duration: 0.2, ease: "easeInOut" }}
       ></motion.div>
       <motion.div
         className="fixed top-0 botton-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
@@ -43,7 +43,7 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
+        transition={{ delay: 0.4, duration: 0.2, ease: "easeInOut" }}
       ></motion.div>
     </>
   );
